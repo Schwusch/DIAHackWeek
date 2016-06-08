f6befa4e6d1d9cf981edfb88551f7b9351e382b5
Jonathan <jonathan.e.m.bocker@gmail.com>
2016-06-08 13:05:24 +0200

    Nu funkar kartan med POIS, paths etc

--- js/projectjs.js
+++ js/projectjs.js
@@ -1,47 +1,52 @@
-$(document).ready(function(){
-    navigator.geolocation.getCurrentPosition(showPosition,
-      errorCallback,
+$(window).load(function(){
+  navigator.geolocation.getCurrentPosition(showPosition,
+    errorCallback,
     {
-        maximumAge: Infinity,
-        timeout:0
+      maximumAge: Infinity,
+      timeout:10000
     });
-});
+  });
 
-function errorCallback(error) {
+  function errorCallback(error) {
     if (error.code == error.PERMISSION_DENIED) {
-        createMap(12.99, 55.61);
+      createMap(12.99, 55.61);
     }
-};
+  };
 
-function showPosition(position) {
+  function showPosition(position) {
     createMap(position.coords.longitude, position.coords.latitude);
-};
+  };
 
-function createMap(x, y)
-{
-  var token = 'pk.eyJ1Ijoic2Nod3VzY2giLCJhIjoiY2lwNWY5MnR2MDAxZncwbTI1NXI3aDY1cyJ9.X3yn1wtjHQ3Q5uDN_WVFKg'
-  mapboxgl.accessToken = token;
-  var map = new mapboxgl.Map({
-      container: 'mapid', // container id
-      style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
-      //center: [12.99, 55.61], // starting position
-      center: [x, y], // starting position
-      zoom: 13.5 // starting zoom
-  });
-  getPlaces(function(results){
+  function createMap(x, y)
+  {
+    var token = 'pk.eyJ1Ijoic2Nod3VzY2giLCJhIjoiY2lwNWY5MnR2MDAxZncwbTI1NXI3aDY1cyJ9.X3yn1wtjHQ3Q5uDN_WVFKg'
     L.mapbox.accessToken = token;
-    L.mapbox.map('mapid', 'mapbox.streets')
-    .featureLayer.setGeoJSON(results);
-  });
-};
+    var map = L.mapbox.map('mapid', 'mapbox.streets')
+    .setView([y, x], 14);
 
-function getPlaces(done){
-  $.ajax({
-          type: "GET",
-          url: "http://build.dia.mah.se/pois?latitude=12.99&longitude=55.61"
-        })
-          .done(function( jsonResult ) {
-            console.log(jsonResult);
-            done(jsonResult.results);
-});
-}
+    getPlaces(x, y, "places", function(results){
+      L.mapbox.featureLayer(results)
+      .addTo(map);
+    });
+
+    getPlaces(x, y, "pois", function(results){
+      L.mapbox.featureLayer(results)
+      .addTo(map);
+    });
+
+    getPlaces(x, y, "paths", function(results){
+      L.mapbox.featureLayer(results)
+      .addTo(map);
+    });
+  };
+
+  function getPlaces(x, y, contentType, whenDone){
+    $.ajax({
+      type: "GET",
+      url: "http://build.dia.mah.se/" + contentType + "?latitude="+ y +"&longitude=" + x + "&within=2000"
+    })
+    .done(function(jsonResult) {
+      console.log(jsonResult);
+      whenDone(jsonResult.results);
+    });
+  }
